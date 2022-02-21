8f352b741d8411403361c4a1f1cdb7a2283a3c9f
Nadia920 <kabaknada64@gmail.com>
2022-02-21 11:15:49 +0300

    add classes in folder controller (signInController, logInController, UserController, MenuController, AdminController, IncidentController))

---
+++ Incidents/src/main/java/com/java/Incidents/controller/CommonController.java
@@ -0,0 +1,12 @@
+package com.java.Incidents.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface CommonController<E extends AbstractEntity> {
+
+    @PostMapping
+    ResponseEntity<E> save(@RequestBody E entity);
+
+}
---
+++ Incidents/src/main/java/com/java/Incidents/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.java.Incidents.controller;
+
+public class UserController {
+}
