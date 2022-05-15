aeb4494f342463bbbfcb8da676e35739395f881b
Nadia920 <82239274+Nadia920@users.noreply.github.com>
2022-05-15 17:31:40 +0300

    Travel

--- Travel/Travel/src/main/java/com/java/Travel/exception/OrderSeatsException.java
+++
@@ -1,9 +0,0 @@
-package com.java.Travel.exception;
-
-public class OrderSeatsException extends RuntimeException {
-
-    public OrderSeatsException(String msg) {
-        super(msg);
-    }
-
-}
--- Travel/src/main/java/com/java/Travel/exception/PaymentException.java
+++
@@ -1,8 +0,0 @@
-package com.java.Travel.exception;
-
-public class PaymentException extends RuntimeException {
-
-    public PaymentException(String s) {
-        super(s);
-    }
-}
